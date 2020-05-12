ac665b9766159cbc64b13571d84b13224a9167ea
Gabriellen Oliveira <gabriellen.oliveira@mbg.local>
2020-05-12 16:06:09 -0300

    tabela livro adicionada

--- sql/banco_livraria(2).sql
+++ sql/banco_livraria(2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12-Maio-2020 às 20:16
+-- Tempo de geração: 12-Maio-2020 às 21:04
 -- Versão do servidor: 10.4.11-MariaDB
 -- versão do PHP: 7.4.3
 
@@ -37,6 +37,26 @@ CREATE TABLE `categoria` (
   `cat_descricao` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `livro`
+--
+
+CREATE TABLE `livro` (
+  `liv_id` int(11) NOT NULL,
+  `cat_id` int(11) DEFAULT NULL,
+  `liv_titulo` varchar(255) DEFAULT NULL,
+  `liv_autor` varchar(255) DEFAULT NULL,
+  `liv_status` varchar(255) DEFAULT NULL,
+  `liv_num_pagina` int(11) DEFAULT NULL,
+  `liv_num_edicao` int(11) DEFAULT NULL,
+  `liv_editora` varchar(200) DEFAULT NULL,
+  `liv_data_criacao` datetime DEFAULT NULL,
+  `liv_data_modificacao` datetime DEFAULT NULL,
+  `liv_descricao` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Índices para tabelas despejadas
 --
@@ -48,6 +68,13 @@ ALTER TABLE `categoria`
   ADD PRIMARY KEY (`cat_id`);
 
 --
+-- Índices para tabela `livro`
+--
+ALTER TABLE `livro`
+  ADD PRIMARY KEY (`liv_id`),
+  ADD KEY `fk_LivCat` (`cat_id`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
@@ -56,6 +83,22 @@ ALTER TABLE `categoria`
 --
 ALTER TABLE `categoria`
   MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `livro`
+--
+ALTER TABLE `livro`
+  MODIFY `liv_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `livro`
+--
+ALTER TABLE `livro`
+  ADD CONSTRAINT `fk_LivCat` FOREIGN KEY (`cat_id`) REFERENCES `categoria` (`cat_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
